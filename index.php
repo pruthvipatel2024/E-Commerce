c603ee8a5f7ca0a6a26aa9c7a50c3c2b3f3b8185
pruthvipatel2024 <patelpruthvi671@gmail.com>
2026-01-05 21:17:46 +0530

    first push

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mage Market</title>
+</head>
+<body>
+    <div><?php include_once("include/menu.php")?></div>
+    <div>search and header</div>
+    <div>body</div>
+    <div>footer</div>    
+</body>
+</html>
