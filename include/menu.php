b1fda4b66a8d66d085fa1565a2033f70f382f7e4
pruthvipatel2024 <patelpruthvi671@gmail.com>
2026-01-06 10:43:04 +0530

    day-1

--- include/menu.php
+++ include/menu.php
@@ -1,13 +1,13 @@
 <div id="menu">
         <ul>
-            <li><a href=><i class="fa fa-home"></i> Home</a></li>
-            <li><a href=><i class="fa fa-book"></i> Lecture</a></li>
-            <li><a href=><i class="fa fa-layer-group"></i> Batch</a></li>
-            <li><a href=><i class="fa fa-book-open"></i> Subject</a></li>
-            <li><a href=><i class="fa fa-chalkboard-teacher"></i> Teacher</a></li>
-            <li><a href=><i class="fa fa-chalkboard"></i> Course</a></li>
-            <li><a href=><i class="fa fa-money-check-alt"></i> Payment</a></li>
-            <li><a href=><i class="fa fa-chart-bar"></i> Reports</a></li>
-            <li><a href=><i class="fa fa-sign-out-alt"></i> Logout</a></li>
+            <li><a href=><i class="fa fa-home"></i> </a></li>
+            <li><a href=><i class="fa fa-book"></i> </a></li>
+            <li><a href=><i class="fa fa-layer-group"></i></a></li>
+            <li><a href=><i class="fa fa-book-open"></i></a></li>
+            <li><a href=><i class="fa fa-chalkboard-teacher"></i></a></li>
+            <li><a href=><i class="fa fa-chalkboard"></i></a></li>
+            <li><a href=><i class="fa fa-money-check-alt"></i></a></li>
+            <li><a href=><i class="fa fa-chart-bar"></i></a></li>
+            <li><a href=><i class="fa fa-sign-out-alt"></i></a></li>
         </ul>
 </div>
